86afafe67bfa79682a1b1b821411a19ab9572d24
cmosley <cmosley@beholdmedia.com>
2021-05-07 10:05:28 -0500

    user auth stubbed, templating structure, admin/urls, profile model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model): 
+  total_bets: models.IntegerField()
+  total_wins: models.IntegerField()
+  bank_roll: models.IntegerField()
+  winnings: models.DecimalField(_(""), max_digits=10, decimal_places=2)
+
+
